2a6c833fb8c86ab61d79f06cf6de972fd8a37ed1
Anmol Kumar Ojha <ojhaanmol@gmail.com>
2024-04-06 00:08:00 +0530

    LEARNED : text and background

---
+++ src/app/background/page.tsx
@@ -0,0 +1,8 @@
+const background = () => {
+    return <div>
+                <div className="align-top bg-gray-500 text-white" >div : align-top bg-gray-500 text-white</div>
+        <div className="align-top bg-black text-white inline-block" >div : align-top bg-black text-white inline-block</div>
+        <section className="align-top bg-gray-500 text-white" >Section : align-top bg-gray-500 text-white</section>
+    </div>
+}
+export default background;
---
+++ src/app/playText/page.tsx
@@ -0,0 +1,11 @@
+const text_tag_01 =  () => {
+    return <div>
+        <p className="text-base text-gray-800 font-extrabold px-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, eius magni inventore quia quasi nihil voluptate repudiandae neque in doloremque animi voluptatum, similique id excepturi? Explicabo quod iusto facilis molestias?</p>
+        <h1 className="text-center text-ellipsis font-bold uppercase mx-px">Headding 1</h1>
+        <h2 className="text-center text-9xl font-bold lowercase mx-px">Headding 2</h2>
+        <h3 className="text-center  font-bold uppercase mx-px">Headding 1</h3>
+        <h4 className="text-center text-7xl font-bold mx-px">Headding 2</h4>
+
+    </div>
+}
+export default text_tag_01
